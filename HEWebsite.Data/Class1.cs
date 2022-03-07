9ff87656c6fa8c9879dcc3452905a87ee54e62f9
Luke <Luke@DESKTOP-IIV6NCO>
2022-03-07 09:02:37 +0000

    Add project files.

---
+++ HEWebsite.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HEWebsite.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ HEWebsite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HEWebsite.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HEWebsite.Areas.Identity.IdentityHostingStartup))]
+namespace HEWebsite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
