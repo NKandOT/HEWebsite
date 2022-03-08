739103c82c7ab427370a0820473fbe020ab2afb8
Luke Payne <380592@students.chesterfield.ac.uk>
2022-03-08 11:53:34 +0000

    Initial Forum Service, Interface, Controller, Models, and View made

---
+++ HEWebsite/Controllers/ForumController.cs
@@ -0,0 +1,37 @@
+﻿using HEWebsite.Data.Interface;
+using HEWebsite.Data.Models;
+using HEWebsite.Models.Forum;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HEWebsite.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+        
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll()
+                .Select(forum => new ForumListingModel {
+                    Id = forum.Id,
+                    Title = forum.Title,
+                    Description = forum.Description
+                });
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ HEWebsite/Models/Forum/ForumIndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HEWebsite.Models.Forum
+{
+    public class ForumIndexModel
+    {
+        public IEnumerable<ForumListingModel> ForumList { get; set; }
+    }
+}
